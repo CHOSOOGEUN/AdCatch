fdac6fd9295524dbd2035b083e25569abc60a49e
jihyun808 <jihyun8k@gmail.com>
2026-03-30 03:33:12 +0900

    feat: setup frontend environment

---
+++ frontend/src/api/axios.ts
@@ -0,0 +1,31 @@
+import axios from 'axios'
+
+const api = axios.create({
+  baseURL: 'http://localhost:8000',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+
+// 요청할 때마다 토큰 자동으로 헤더에 추가
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token') || sessionStorage.getItem('token')
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+})
+
+// 토큰 만료(401) 시 로그인 페이지로 이동
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem('token')
+      window.location.href = '/'
+    }
+    return Promise.reject(error)
+  }
+)
+
+export default api
